e4eed33fdcf1f2ad824a8199b7811c08402aa853
Daniel Yang <danielyangkang@gmail.com>
2024-05-01 23:16:38 -0700

    find case insensitive

--- init.lua
+++ init.lua
@@ -1,13 +1,17 @@
 require("config")
 require("config.remap")
 require("config.packer")
-vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+vim.opt.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.opt.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 2 -- Number of spaces inserted when indenting
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.wo.number = true
 vim.opt.fillchars = {eob = " "}
 vim.opt.virtualedit:append("onemore")
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.cmd([[
   augroup MoveCursorOnInsertLeave
     autocmd!
