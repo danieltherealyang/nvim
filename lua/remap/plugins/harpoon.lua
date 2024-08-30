7b284df1cdad7dbfbd3bb915ea957c8692ea5ae7
Daniel Yang <danielyangkang@gmail.com>
2024-08-30 14:16:09 -0700

    remove entries from harpoon menu

--- lua/remap/plugins/harpoon.lua
+++ lua/remap/plugins/harpoon.lua
@@ -7,22 +7,37 @@ harpoon:setup()
 -- basic telescope configuration
 local conf = require("telescope.config").values
 local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
+    local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+            table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+            results = paths,
+        })
     end
 
     require("telescope.pickers").new({}, {
         prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
+        finder = finder(),
         sorter = conf.generic_sorter({}),
+        previewer = conf.file_previewer({}),
+        attach_mappings = function(prompt_bufnr, map)
+            map("n", "dd", function()
+                local state = require("telescope.actions.state")
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_bufnr)
+
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
+            end)
+            return true
+        end,
     }):find()
 end
 
-vim.keymap.set("n", "<Leader>a", function() harpoon:list():add() end, { desc = "Harpoon add buffer" })
+vim.keymap.set("n", "<Leader>a", function() harpoon:list():add() print("Added to Harpoon") end, { desc = "Harpoon add buffer" })
 vim.keymap.set("n", "<Leader><Tab>", function() toggle_telescope(harpoon:list()) end, { desc = "Harpoon open menu" })
 
 -- Toggle previous & next buffers stored within Harpoon list
