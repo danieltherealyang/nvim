d2b24ef5206aa33c7a7eb376c947c8d39416fb06
Daniel Yang <danielyangkang@gmail.com>
2024-12-31 23:28:28 -0800

    Added theme switching, edited desc capitalization

--- lua/remap/plugins/windows.lua
+++ lua/remap/plugins/windows.lua
@@ -2,7 +2,7 @@ local function cmd(command)
    return table.concat({ '<Cmd>', command, '<CR>' })
 end
 
-vim.keymap.set('n', '<Leader>+', cmd 'WindowsMaximize', { desc = "Windows Max"})
-vim.keymap.set('n', '<Leader>_', cmd 'WindowsMaximizeVertically', { desc = "Windows Vert Max" })
-vim.keymap.set('n', '<Leader>|', cmd 'WindowsMaximizeHorizontally', { desc = "Windows Hor Max" })
-vim.keymap.set('n', '<Leader>=', cmd 'WindowsEqualize', { desc = "Windows Equalize" })
+vim.keymap.set('n', '<Leader>+', cmd 'WindowsMaximize', { desc = "Windows max"})
+vim.keymap.set('n', '<Leader>_', cmd 'WindowsMaximizeVertically', { desc = "Windows vert max" })
+vim.keymap.set('n', '<Leader>|', cmd 'WindowsMaximizeHorizontally', { desc = "Windows hor max" })
+vim.keymap.set('n', '<Leader>=', cmd 'WindowsEqualize', { desc = "Windows equalize" })
