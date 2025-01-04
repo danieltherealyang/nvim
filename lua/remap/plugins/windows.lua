9fd266ee408ed26f92870c068a264b1dfb37691c
Daniel Yang <danielyangkang@gmail.com>
2025-01-04 01:23:29 -0800

    changed keymap to toggle window resizing

--- lua/remap/plugins/windows.lua
+++ lua/remap/plugins/windows.lua
@@ -2,7 +2,7 @@ local function cmd(command)
    return table.concat({ '<Cmd>', command, '<CR>' })
 end
 
-vim.keymap.set('n', '<Leader>+', cmd 'WindowsMaximize', { desc = "Windows max"})
-vim.keymap.set('n', '<Leader>_', cmd 'WindowsMaximizeVertically', { desc = "Windows vert max" })
-vim.keymap.set('n', '<Leader>|', cmd 'WindowsMaximizeHorizontally', { desc = "Windows hor max" })
-vim.keymap.set('n', '<Leader>=', cmd 'WindowsEqualize', { desc = "Windows equalize" })
+vim.keymap.set('n', '<Leader>+', cmd 'WindowsMaximize', { desc = "Windows maximize"})
+vim.keymap.set('n', '<Leader>_', cmd 'WindowsMaximizeVertically', { desc = "Windows vertical maximize" })
+vim.keymap.set('n', '<Leader>|', cmd 'WindowsToggleAutowidth', { desc = "Windows toggle resizing" })
+vim.keymap.set('n', '<Leader>=', cmd 'WindowsEqualize', { desc = "Windows communism" })
