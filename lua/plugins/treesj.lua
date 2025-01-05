c82c5132a6ba3bd0ed5cfec55e107cb4e809f180
Daniel Yang <danielyangkang@gmail.com>
2025-01-04 17:06:12 -0800

    Added desc to TreeSJ keys

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,6 +1,10 @@
 return {
     'Wansmer/treesj',
-    keys = { '<Leader>m', '<Leader>j', '<Leader>s' },
+    keys = {
+        { "<Leader>m", desc = "Toggle block" },
+        { "<Leader>j", desc = "Join block" },
+        { "<Leader>s", desc = "Split block" },
+    },
     dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
     config = function()
         require('treesj').setup({--[[ your config ]]})
