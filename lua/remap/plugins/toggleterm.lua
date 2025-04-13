b5e9b2784630d436b9a0955e957dd03f4157c6a4
Daniel Yang <danielyangkang@gmail.com>
2025-04-12 23:26:31 -0700

    Fix toggleterm errors

--- lua/remap/plugins/toggleterm.lua
+++ lua/remap/plugins/toggleterm.lua
@@ -2,9 +2,9 @@ require('toggleterm').setup{
     autoscroll = false,
 }
 
-vim.keymap.set('t', '<C-q>', '<C-\\><C-n>:ToggleTerm<CR>', { silent = true, desc = "Toggle terminal" })
-vim.keymap.set('n', '<C-q>', ':ToggleTerm<CR>', { silent = true, desc = "Toggle terminal" })
-vim.keymap.set('i', '<C-q>', '<C-\\><C-n>:ToggleTerm<CR>', { silent = true, desc = "Toggle terminal" })
-vim.keymap.set('n', '<C-Space>h', ':ToggleTerm direction=horizontal<CR>', { silent = true, desc = "Toggle horizontal terminal" })
-vim.keymap.set('n', '<C-Space>f', ':ToggleTerm direction=float<CR>', { silent = true, desc = "Toggle floating terminal" })
-vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { noremap = true, desc = "Exit terminal mode" })
+vim.keymap.set('t', '<C-q>', [[<C-\><C-n><CMD>ToggleTerm<CR>]], { silent = true, desc = "Toggle terminal" })
+vim.keymap.set('n', '<C-q>', [[<CMD>ToggleTerm<CR>]], { silent = true, desc = "Toggle terminal" })
+vim.keymap.set('i', '<C-q>', [[<C-\><C-n><CMD>ToggleTerm<CR>]], { silent = true, desc = "Toggle terminal" })
+vim.keymap.set('n', '<C-Space>h', [[<CMD>ToggleTerm direction=horizontal<CR>]], { silent = true, desc = "Toggle horizontal terminal" })
+vim.keymap.set('n', '<C-Space>f', [[<CMD>ToggleTerm direction=float<CR>]], { silent = true, desc = "Toggle floating terminal" })
+vim.keymap.set('t', '<ESC>', [[<C-\><C-n>]], { silent = true, desc = "Exit terminal mode" })
